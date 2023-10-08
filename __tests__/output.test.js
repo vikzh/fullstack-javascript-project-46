59aa1e417c386f28eaaa09f6d39d442d7f371ad0
vikzh <vikt.anmail@gmail.com>
2023-10-08 16:16:01 +0200

    test adjusted

--- __tests__/output.test.js
+++ __tests__/output.test.js
@@ -1,12 +1,31 @@
 import fs from 'fs';
 import genDiffJson from '../src/compare.js';
 
-test('json recursive test', () => {
-  expect(genDiffJson('./__fixtures__/file1_nested.json', './__fixtures__/file2_nested.json'))
-    .toBe((fs.readFileSync('./__fixtures__/file1_file2_nested_stylish_result')).toString());
+const buildFixturePath = (fileName) => {
+  const fixtureDirectory = './__fixtures__/';
+
+  return `${fixtureDirectory}${fileName}`;
+};
+
+const extensions = ['json', 'yml'];
+const stylishResult = fs.readFileSync(buildFixturePath('file1_file2_nested_stylish_result')).toString();
+const plainResult = fs.readFileSync(buildFixturePath('file1_file2_nested_plain_result')).toString();
+const file1NestedName = 'file1_nested';
+const file2NestedName = 'file2_nested';
+
+test.each([
+  [stylishResult, 'stylish'],
+  [plainResult, 'plain']])('nestedFileTest', (expectedResult, format) => {
+  const [jsonExtension, ymlExtension] = extensions;
+  console.log('result !!!!!', genDiffJson(buildFixturePath(`${file1NestedName}.${jsonExtension}`), buildFixturePath(`${file2NestedName}.${jsonExtension}`), format));
+  expect(genDiffJson(buildFixturePath(`${file1NestedName}.${jsonExtension}`), buildFixturePath(`${file2NestedName}.${jsonExtension}`), format))
+    .toBe(expectedResult);
+  expect(genDiffJson(buildFixturePath(`${file1NestedName}.${ymlExtension}`), buildFixturePath(`${file2NestedName}.${ymlExtension}`), format))
+    .toBe(expectedResult);
 });
 
-test('json recursive plain formating test', () => {
-  expect(genDiffJson('./__fixtures__/file1_nested.json', './__fixtures__/file2_nested.json', 'plain'))
-    .toBe((fs.readFileSync('./__fixtures__/file1_file2_nested_plain_result')).toString());
+test('test default format', () => {
+  const [jsonExtension] = extensions;
+  expect(genDiffJson(buildFixturePath(`${file1NestedName}.${jsonExtension}`), buildFixturePath(`${file2NestedName}.${jsonExtension}`)))
+    .toBe(stylishResult);
 });
